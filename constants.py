ac9d5dfadaf206b4c3b5c6d5a3e5e567fba12777
suyashLade <suyashlade@gmail.com>
2023-12-19 17:12:27 +0530

    adding evn config for my system

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
 import os
 from chromadb.config import Settings 
-
+from  chromadb import chromadb as Chroma
 CHROMA_SETTINGS=Settings(
     chroma_db_impl='duckdb+parquet',
     persist_directory='db',
     anonymized_telemetry = False
 )
+
