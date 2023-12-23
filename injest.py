d1e2c2c67411defd3756407481afa3ddde21b2c9
suyashLade <suyashlade@gmail.com>
2023-12-23 22:06:54 +0530

    tried markdown files injest

--- injest.py
+++ injest.py
@@ -4,18 +4,22 @@ from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import Chroma 
 import os 
 from constants import CHROMA_SETTINGS
+from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain.document_loaders import UnstructuredMarkdownLoader
 
 persist_directory = "db"
 
 def main():
+
     for root, dirs, files in os.walk("docs"):
         for file in files:
-            if file.endswith(".pdf"):
+            print(file)
+            if file.endswith(".md"):
                             print(file)
-                            loader = PDFMinerLoader(os.path.join(root, file))
+                            loader = UnstructuredMarkdownLoader(os.path.join(root, file))
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=500)
-    texts = text_splitter.split_documents(documents)
+    texts = text_splitter.split_documents(loader)
     #create embeddings here
     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
     #create vector store here
